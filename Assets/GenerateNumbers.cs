2c9d839449c45a88b6061db7eb946c6a638301e9
DutchRascal <DutchRascal@fritz.box>
2019-09-08 17:55:25 +0200

    20190908-1755

---
+++ Assets/GenerateNumbers.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateNumbers : MonoBehaviour
+{
+    [SerializeField] Text number1;
+    [SerializeField] Text number2;
+    [SerializeField] Text sign;
+    [SerializeField] Text answer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number1.text = Random.Range(0, 20).ToString();
+        number2.text = Random.Range(0, 20).ToString();
+        sign.text = "+";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GetInput()
+    {
+        if (answer.text != "")
+        {
+            print(answer.text);
+        }
+    }
+}
