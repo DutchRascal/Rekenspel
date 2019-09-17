a629eb548cf8981441a05e81a779215adf7f87e0
DutchRascal <DutchRascal@fritz.box>
2019-09-17 22:09:26 +0200

    20190917-2210

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void LoadScene(int scene)
+    {
+        switch (scene)
+        {
+            case 1:
+                SceneManager.LoadScene("Add");
+                break;
+            case 2:
+                SceneManager.LoadScene("Substract");
+                break;
+            case 3:
+                SceneManager.LoadScene("Divide");
+                break;
+        }
+    }
+}
